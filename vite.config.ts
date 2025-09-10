08b1251f6f5e4873a0bad1b69133e72ce0567832
Aish Maheshwari <155080572+stillaish@users.noreply.github.com>
2025-09-10 11:13:25 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.',
   build: {
-    outDir: 'dist'
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
